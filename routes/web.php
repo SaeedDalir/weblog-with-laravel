4d0d053b00706e7d049463ebd24aaba28b1e85f2
Saeed <saeedkingdr@gmail.com>
2019-05-13 10:59:33 +0430

    delete unnecessary lines and routes in web routes

--- routes/web.php
+++ routes/web.php
@@ -19,20 +19,14 @@ Route::group(['middleware'=>'admin'], function (){
     Route::resource('admin/users','Admin\AdminUserController');
     Route::resource('admin/posts','Admin\AdminPostController');
     Route::delete('admin/delete/post','Admin\AdminPostController@deleteAll')->name('post.delete.all');
-
     Route::resource('admin/categories','Admin\AdminCategoryController');
     Route::resource('admin/photos','Admin\AdminPhotoController');
     Route::delete('admin/delete/media','Admin\AdminPhotoController@deleteAll')->name('photo.delete.all');
     Route::get('admin/dashboard','Admin\DashboardController@index')->name('dashboard.index');
     Route::resource('admin/comments','Admin\CommentController');
     Route::delete('admin/delete/comment','Admin\CommentController@deleteAll')->name('comment.delete.all');
-
     Route::post('admin/actions/{id}','Admin\CommentController@actions')->name('comments.actions');
 });
-Route::group(['middleware'=>'writer'], function (){
-});
-
-
 Route::get('/','Frontend\MainController@index');
 Route::get('/posts/{slug}','Frontend\PostController@show')->name('frontend.posts.show');
 Route::get('/search','Frontend\PostController@searchTitle')->name('frontend.posts.search');
